b05ca6759e047960183c844f7367dc3b1e3733b4
Jujube Hutt <jhutt27@students.polytechnic.org>
2026-04-26 11:50:48 -0700

    Add BinaryHeap (minheap implementation)

--- atds.py
+++ atds.py
@@ -2,7 +2,7 @@
 """
 atds.py
 Advanced Topics Data Structures
-A collection of data types including: Stacks, Queues, Deques
+A collection of data types including: Stacks, Queues, Deques, Nodes, Trees, Heaps, etc.
 """
 
 __author__ = "Jujube Hutt"
@@ -390,7 +390,7 @@ class HashTable(object):
         return self.entries
 
 class BinaryTree(object):
-    def __init__(self, value, left = None, right = None):
+    def __init__(self, value = None, left = None, right = None):
         """Constructs the BinaryTree with value, left_child, and right_child"""
         self.value = value
         self.left_child = left
@@ -419,86 +419,222 @@ class BinaryTree(object):
     def __repr__(self):
         return "BinaryTree[key=" + str(self.value) + ",left_child=" + str(self.left_child) + ",right_child=" + str(self.right_child) + "]"
     
+class BinaryHeap():
+    """The BinaryHeap class implements the Binary Heap Abstract 
+    Data Type as a list of values, where the index p of a parent
+    can be calculated from the index c of a child as c // 2.
+    """
+    def __init__(self):
+        self.heap_list = [0]  # not used. Here just to make parent-
+                             # child calculations work nicely.
+        # Note that current size of heap = len(self.heapList) - 1
+
+    def insert(self,value):
+        """Inserts a value into the heap by:
+        a. adding it to the end of the list, and then
+        b. "percolating" it up to an appropriate position
+        """
+        self.heap_list.append(value)
+        self.percolate_up(len(self.heap_list)-1)
+
+    def percolate_up(self, i):
+        """Beginning at i, check to see if parent above is greater than
+        value at i. If so, percolate i upwards to parent's position.
+        """
+        if i % 2 == 0:
+            while i > 1 and self.heap_list[i] < self.heap_list[i//2]:
+                self.heap_list[i], self.heap_list[i//2] = self.heap_list[i//2], self.heap_list[i]
+                self.percolate_up(i // 2)
+        else:
+            while i > 1 and self.heap_list[i] < self.heap_list[(i-1)//2]:
+                self.heap_list[i], self.heap_list[(i-1)//2] = self.heap_list[(i-1)//2], self.heap_list[i]
+                self.percolate_up((i-1)//2)
+
+            
+
+    def del_min(self):
+        """Removes the minimum value and returns it, along with reordering the heap so it is still valid.
+        """
+        thing = self.heap_list[1]
+        self.heap_list[1], self.heap_list[-1] = self.heap_list[-1], self.heap_list[1]
+        self.heap_list.remove(thing)
+        self.percolate_down(1)
+        return thing
+
+    def percolate_down(self,i):
+        """Moves the item at i down to a correct level in the heap. To
+        work correctly, needs to identify the minimum child for parent i.
+        """
+        while 2*i + 1 < len(self.heap_list) and (self.heap_list[i] > self.heap_list[2*i] or self.heap_list[i] > self.heap_list[2*i+1]):
+            if self.heap_list[2*i] < self.heap_list[2*i+1]:
+                self.heap_list[i], self.heap_list[2*i] = self.heap_list[2*i], self.heap_list[i]
+                self.percolate_down(2*i)
+            else:
+                self.heap_list[i], self.heap_list[2*i+1] = self.heap_list[2*i+1], self.heap_list[i]
+                self.percolate_down(2*i+1)
+        if 2*i < len(self.heap_list) and self.heap_list[i] > self.heap_list[2*i]:
+            self.heap_list[i], self.heap_list[2*i] = self.heap_list[2*i], self.heap_list[i]
+            
+
+
+
+    def find_min(self):
+        """Returns the minimum item in the heap, without removing it.
+        """
+        if len(self.heap_list) == 1:
+            return None
+        else:
+            return self.heap_list[1]
+
+
+    def is_empty(self):
+        return len(self.heap_list) - 1 == 0
+
+    def size(self):
+        return len(self.heap_list) - 1
+
+    def build_heap(self, list_of_keys):
+        """Returns a new heap based on a pre-existing list of key 
+        values."""
+        for key in list_of_keys:
+            self.insert(key)
+
+    def __repr__(self):
+        return "BinaryHeap" + str(self.heap_list)
 
 def main():
 
-    bs = BinarySearcher()
+    print("Testing minHeap binary tree")
     tests_passed = 0
-    tests = [
-        # (array, value_to_search, expected_result)
-        ([1, 2, 3, 4, 7, 9, 13, 14, 20], 7, 4),
-        ([1, 2, 3, 4, 7, 9, 13, 14, 20], 1, 0),
-        ([1, 2, 3, 4, 7, 9, 13, 14, 20], 20, 8),
-        ([1, 2, 3, 4, 7, 9, 13, 14, 20], -2, None),
-        ([1, 2, 3, 4, 7, 9, 13, 14, 20], 23, None),
-        ([1, 2, 3, 4, 7, 9, 13, 14, 20], 10, None),
-        ([4, 7, 9, 13, 14, 20], 9, 2),
-        ([4, 7, 9, 13, 14, 20], 13, 3),
-        ([4, 7, 9, 13, 14, 20], 20, 5),
-        ([4, 7, 9, 13, 14, 20], 2, None),
-        ([4, 7, 9, 13, 14, 20], 10, None),
-        ([4, 7, 9, 13, 14, 20], 22, None),
-    ]
-
-    for arr, value, expected in tests:
-        result = bs.search(arr, value)
-        if result == expected:
-            print(f"PASS: {value} in {arr} -> {result}")
+    
+    # Test 1 - Creating a binary heap
+    print("Instantiating empty BinaryTree object...")
+    try:
+        bh = BinaryHeap()
+        tests_passed += 1
+        print("Test 1 passed!")
+    except:
+        print("Issue creating BinaryTree")
+    print(str(tests_passed) + "/" + str(10))
+    
+    # Test 2 - printing the heap
+    print("Printing the heap...")
+    try:
+        print(bh)
+        tests_passed += 1
+        print("Test 2 passed")
+    except:
+        print("Issue calling __repr__")
+    print(str(tests_passed) + "/" + str(10))
+    
+    # Test 3 - inserting a key
+    print("Inserting a key...")
+    try:
+        bh.insert(5)
+        tests_passed += 1
+        print("Test 3 passed!")
+    except:
+        print("Issue creating a node")
+    print(str(tests_passed) + "/" + str(10))
+    
+    # Test 4 - inserting more key
+    print("Inserting keys 7, 9, and 3...")
+    try:
+        bh.insert(7)
+        bh.insert(9)
+        bh.insert(3)
+        print("New heap:",bh)
+        if bh.__repr__() == "BinaryHeap[0, 3, 5, 9, 7]":
             tests_passed += 1
+            print("Test 4 passed!")
         else:
-            print(f"FAIL: {value} in {arr} -> got {result}, expected {expected}")
-    print(f"Tests passed: {tests_passed}/{len(tests)}")
+            print("Keys inserted incorrectly")
+            print("Expected: BinaryHeap[0, 3, 5, 9, 7]")
+    except:
+        print("Issue with inserting keys correctly")
+    print(str(tests_passed) + "/" + str(10))
+ 
+    # Test 5 - Viewing min value
+    print("Checking findMin() method...")
+    try:
+        min_val = bh.find_min()
+        if min_val == 3:
+            tests_passed += 1
+            print("Test 5 passed!")
+        else:
+            print("Incorrect min_value found")
+    except:
+        print("Issue calling the find_min() method")
+    print(str(tests_passed) + "/" + str(10))
+    
+    # Test 6 - Removing a value
+    print("Calling the del_min() method")
+    try:
+        val = bh.del_min()
+        if val == 3:
+            tests_passed += 1
+            print("Test 6 passed!")
+        else:
+            print("Minimum value not returned")
+    except:
+        print("Issue deleting the minimum value")
+    print(str(tests_passed) + "/" + str(10))
+    
+    # Test 7 - Checking heap structure
+    print("Checking heap structure after deletion...")
+    try:
+        result = bh.__repr__()
+        if result == "BinaryHeap[0, 5, 7, 9]":
+            tests_passed += 1
+            print("Test 7 passed!")
+        else:
+            print("Expected BinaryHeap[0, 5, 7, 9]")
+            print("Deleting the minimum messed up the heap")
+    except:
+        print("Issue with print out heap structure")
+    print(str(tests_passed) + "/" + str(10))
+    
+    # Test 8 - checking stack
+    print("Checking if heap is empty with .is_empty() and .size()...")
+    try:
+        if not bh.is_empty() and bh.size() == 3:
+            tests_passed += 1
+            print("Test 8 passed!")
+        else:
+            print("Problem with .is_empty() or .size()")
+    except:
+        print("Issue with .is_empty() or .size()")
+    print(str(tests_passed) + "/" + str(10))
+    
+    # Test 9 - inserting a key
+    print("Emptying stack and checking .is_empty()...")
+        
+    bh.del_min()
+    bh.del_min()
+    bh.del_min()
+    if bh.is_empty() and bh.size() == 0:
+        tests_passed += 1
+        print("Test 9 passed!")
+    else:
+        print("Issue with .is_empty() or .size()")
+    '''
+    except:
+        print("Issue with .is_empty() or .size()")
+    '''
+    print(str(tests_passed) + "/" + str(10))
+    
 
-    tests_passed = 0
-    ls = LinearSearcher()
-    print("Testing linear_search.py, search(lst, key) function")
-    print("LearnSearcher found")
-    tests_passed += 1
-    l = [20,3,13,4,7,9,7,14,1]
-    print("Initial list:", l)
-    print("Searching for item at beginning,", l[0], end='...')
-    if ls.search(l,l[0]) == 0:
-        print("passed")
+    # Test 10 - inserting a key
+    print("Testing buildHeap method...")
+    values = [2, 3, 9, 4, 1, 8, 7, 15, 20, 41, 32, 5]
+    print("Array:",values)
+    print("Building heap...")
+    bh.build_heap(values)
+    print("Resulting heap:",bh)
+    if bh.__repr__() == "BinaryHeap[0, 1, 2, 5, 4, 3, 8, 7, 15, 20, 41, 32, 9]": # Mine doesn't match this one (it has the 7 and 8 flipped) but it is still a valid minheap
         tests_passed += 1
-    else:
-        print("failed")
-    print("Searching for 23 (not on list)", end='...')
-    if ls.search(l,23) == None:
-        print("passed")
-        tests_passed += 1
-    else:
-        print("failed. Should have indicated None")
-    print("Searching for item at end,", l[-1], end='...')
-    if ls.search(l,l[-1]) == len(l) - 1:
-        print("passed")
-        tests_passed += 1
-    else:
-        print("failed")
-    print("Tests passed:" + str(tests_passed) + "/4")  
-
-    """
-    #Testing Binary searching 
-    b = BinarySearcher()
-    nums = [3,5,7,20,26,90,100]
-    print("Should be None")
-    print(LinearSearcher.search(nums,19))
-    print("Should be 4")
-    print(LinearSearcher.search(nums,26))
-    print("Should be 2")
-    print(BinarySearcher.search(nums,7))
-    print("Should be None")
-    print(BinarySearcherRecursive.search(nums,101,0,len(nums)))
-    print("Should be None") 
-    print(BinarySearcherRecursive.search(nums,1,0,len(nums)))
-    print("Should be 0")
-    print(BinarySearcherRecursive.search(nums,3,0,len(nums)))
-    print(BinarySearcherRecursive.search(nums,5,0,len(nums)))
-    print(BinarySearcherRecursive.search(nums,7,0,len(nums)))
-    print(BinarySearcherRecursive.search(nums,20,0,len(nums)))
-    print(BinarySearcherRecursive.search(nums,26,0,len(nums)))
-    print(BinarySearcherRecursive.search(nums,90,0,len(nums)))
-    print(BinarySearcherRecursive.search(nums,100,0,len(nums)))
-    """
+        print("Test 10 passed!")
+    print(str(tests_passed) + "/" + str(10))
 
 if __name__ == "__main__":
     main()
