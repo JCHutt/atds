6e01c87361eb1b13344244bfcbeb5b0bfefa5286
Jujube Hutt <jhutt27@students.polytechnic.org>
2026-05-04 20:15:37 -0700

    Add Graph and Vertex classes

--- atds.py
+++ atds.py
@@ -2,11 +2,11 @@
 """
 atds.py
 Advanced Topics Data Structures
-A collection of data types including: Stacks, Queues, Deques, Nodes, Trees, Heaps, etc.
+A collection of data types including: Stacks, Queues, Deques, Nodes, Trees, Heaps, Graphs, etc.
 """
 
 __author__ = "Jujube Hutt"
-__version__ = "2026-02-18"
+__version__ = "2026-05-04"
 
 class Stack(object):
     """Defines a stack."""
@@ -502,139 +502,123 @@ class BinaryHeap():
     def __repr__(self):
         return "BinaryHeap" + str(self.heap_list)
 
+class Vertex(object):
+    """Describes a vertex object in terms of a "key" and a
+    dictionary that indicates edges to neighboring vertices with
+    a specified weight.
+    """
+    def __init__(self, key):
+        """Constructs a vertex with a key value and an empty dictionary 
+        in which we'll store other vertices to which this vertex is connected.
+        """
+        self.key = key
+        self.neighbors = {}   # empty dictionary for neighboring vertices
+
+    def set_neighbor(self, other, weight=0):
+        """Adds a reference to a neighboring Vertex object to the dictionary, 
+        to which this vertex is connected by an edge. If a weight is not indicated, 
+        default weight is 0.
+        """
+        self.neighbors[other] = weight
+
+    def __repr__(self):
+        """Returns a representation of the vertex and its neighbors, suitable for 
+        printing. Check out the example of 'list comprehension' here!
+        """
+        return f"Vertex({self.key}"
+        
+    def __str__(self):
+        return ( f"{self.key} connected to: " 
+        + f"{[x.key for x in self.neighbors]}")
+
+    def get_neighbors(self):
+        return self.neighbors.keys()
+
+    def get_key(self):
+        return self.key
+
+    def get_neighbor(self, other):
+        """Returns the weight of an edge connecting this vertex with another,
+        or None if the neighbor doesn't exist
+        """
+        return self.neighbors.get(other, None)
+    
+
+class Graph(object):
+    """Describes the Graph class, which is primarily a dictionary
+    mapping vertex names to Vertex objects, along with a few methods
+    that can be used to manipulate them.
+    """
+    def __init__(self):
+        self.vertices = {}
+
+    def set_vertex(self, key):
+        self.vertices[key] = Vertex(key)
+
+    def get_vertex(self, key):
+        """Looks for the key in the dictionary of Vertex objects, and
+        returns the Vertex if found. Otherwise, returns None.
+        """
+        '''
+        # This is the classic way of doing that
+        if key in self.graph.keys():
+            return self.graph[key]
+        else:
+            return None
+        '''
+        # Single-line alternative
+        return self.vertices.get(key, None)
+
+    def __contains__(self, key):
+        """This 'dunder' expression is written so we can use Python's "in"
+        operation: If the parameter 'key' is in the dictionary of vertices,
+        the value of "key in my_graph" will be True, otherwise False.
+        """
+        return key in self.vertices
+
+    def add_edge(self, from_vert, to_vert, weight=0):
+        """Adds an edge connecting two vertices (specified by key
+        parameters) by modifying those vertex objects. Note that
+        the weight can be specified as well, but if one isn't
+        specified, the value of weight will be the default value
+        of 0.
+        """
+        # if the from_key doesn't yet have a vertex, create it
+        if from_vert not in self.vertices:
+            self.set_vertex(from_vert)
+        # if the to_key doesn't yet have a vertex, create it
+        if to_vert not in self.vertices:
+            self.set_vertex(to_vert)
+        # now we can create the edge between the two
+        self.vertices[from_vert].set_neighbor(self.vertices[to_vert], weight)
+
+    def get_vertices(self):
+        return self.vertices.keys()
+
+    def __iter__(self):
+        """Another 'dunder' expression that allows us to iterate through
+        the list of vertices.
+        Example use:
+        for vertex in graph:  # Python understands this now!
+            print(vertex)
+        """
+        return iter(self.vertices.values())
+
 def main():
 
-    print("Testing minHeap binary tree")
-    tests_passed = 0
-    
-    # Test 1 - Creating a binary heap
-    print("Instantiating empty BinaryTree object...")
-    try:
-        bh = BinaryHeap()
-        tests_passed += 1
-        print("Test 1 passed!")
-    except:
-        print("Issue creating BinaryTree")
-    print(str(tests_passed) + "/" + str(10))
-    
-    # Test 2 - printing the heap
-    print("Printing the heap...")
-    try:
-        print(bh)
-        tests_passed += 1
-        print("Test 2 passed")
-    except:
-        print("Issue calling __repr__")
-    print(str(tests_passed) + "/" + str(10))
-    
-    # Test 3 - inserting a key
-    print("Inserting a key...")
-    try:
-        bh.insert(5)
-        tests_passed += 1
-        print("Test 3 passed!")
-    except:
-        print("Issue creating a node")
-    print(str(tests_passed) + "/" + str(10))
-    
-    # Test 4 - inserting more key
-    print("Inserting keys 7, 9, and 3...")
-    try:
-        bh.insert(7)
-        bh.insert(9)
-        bh.insert(3)
-        print("New heap:",bh)
-        if bh.__repr__() == "BinaryHeap[0, 3, 5, 9, 7]":
-            tests_passed += 1
-            print("Test 4 passed!")
-        else:
-            print("Keys inserted incorrectly")
-            print("Expected: BinaryHeap[0, 3, 5, 9, 7]")
-    except:
-        print("Issue with inserting keys correctly")
-    print(str(tests_passed) + "/" + str(10))
- 
-    # Test 5 - Viewing min value
-    print("Checking findMin() method...")
-    try:
-        min_val = bh.find_min()
-        if min_val == 3:
-            tests_passed += 1
-            print("Test 5 passed!")
-        else:
-            print("Incorrect min_value found")
-    except:
-        print("Issue calling the find_min() method")
-    print(str(tests_passed) + "/" + str(10))
-    
-    # Test 6 - Removing a value
-    print("Calling the del_min() method")
-    try:
-        val = bh.del_min()
-        if val == 3:
-            tests_passed += 1
-            print("Test 6 passed!")
-        else:
-            print("Minimum value not returned")
-    except:
-        print("Issue deleting the minimum value")
-    print(str(tests_passed) + "/" + str(10))
-    
-    # Test 7 - Checking heap structure
-    print("Checking heap structure after deletion...")
-    try:
-        result = bh.__repr__()
-        if result == "BinaryHeap[0, 5, 7, 9]":
-            tests_passed += 1
-            print("Test 7 passed!")
-        else:
-            print("Expected BinaryHeap[0, 5, 7, 9]")
-            print("Deleting the minimum messed up the heap")
-    except:
-        print("Issue with print out heap structure")
-    print(str(tests_passed) + "/" + str(10))
-    
-    # Test 8 - checking stack
-    print("Checking if heap is empty with .is_empty() and .size()...")
-    try:
-        if not bh.is_empty() and bh.size() == 3:
-            tests_passed += 1
-            print("Test 8 passed!")
-        else:
-            print("Problem with .is_empty() or .size()")
-    except:
-        print("Issue with .is_empty() or .size()")
-    print(str(tests_passed) + "/" + str(10))
-    
-    # Test 9 - inserting a key
-    print("Emptying stack and checking .is_empty()...")
-        
-    bh.del_min()
-    bh.del_min()
-    bh.del_min()
-    if bh.is_empty() and bh.size() == 0:
-        tests_passed += 1
-        print("Test 9 passed!")
-    else:
-        print("Issue with .is_empty() or .size()")
-    '''
-    except:
-        print("Issue with .is_empty() or .size()")
-    '''
-    print(str(tests_passed) + "/" + str(10))
-    
-
-    # Test 10 - inserting a key
-    print("Testing buildHeap method...")
-    values = [2, 3, 9, 4, 1, 8, 7, 15, 20, 41, 32, 5]
-    print("Array:",values)
-    print("Building heap...")
-    bh.build_heap(values)
-    print("Resulting heap:",bh)
-    if bh.__repr__() == "BinaryHeap[0, 1, 2, 5, 4, 3, 8, 7, 15, 20, 41, 32, 9]": # Mine doesn't match this one (it has the 7 and 8 flipped) but it is still a valid minheap
-        tests_passed += 1
-        print("Test 10 passed!")
-    print(str(tests_passed) + "/" + str(10))
+    g = Graph()
+    g.add_edge("V0", "V1", 2)
+    g.add_edge("V1","V2",1)
+    g.add_edge("V1","V3",5)
+    g.add_edge("V3","V2",7)
+    g.add_edge("V2","V4",2)
+    g.add_edge("V4","V3",4)
+    g.add_edge("V5","V4",6)
+    g.add_edge("V0","V5",12)
+    for vertex in g:
+        print(vertex)
+        for n in vertex.get_neighbors():
+            print("\t", n.get_key(), n.neighbors)
 
 if __name__ == "__main__":
     main()
